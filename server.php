10a3b788c7d80228df4e835a7a998d784e88f2a3
ParitWy <dnavparit3y@gmail.com>
2020-06-08 14:35:31 +0000

    php

--- server.php
+++ server.php
@@ -1,17 +1,4 @@
-<style type=”text/css”>
-.header{
-    font-size: 50px;
-}
-body{
-    background-image: url('photo-1579546929518-9e396f3cc809.jpg');
-    background-position: center;
-   background-repeat: no-repeat;
-   background-size: cover;
-    height: 100%;
-    text-align: center;
-    font-family: 'Roboto',sans-serif;
-}
-</style>
+
 <?php
 $pseudo = $_POST('gmail')
 echo "<h1 class = 'header'>Bienvenue ".$pseudo."</h1>"
